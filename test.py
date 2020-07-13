c87a809736143e387e61afcc765b676d1796b6a1
mgonzalez91 <m.gonzalez@gatech.edu>
2020-07-13 09:40:35 -0400

    resize and recrop

--- test.py
+++ test.py
@@ -4,15 +4,57 @@ import cv2
 import pickle as pkl
 import numpy as np 
 import matplotlib.pyplot as plt
+from libs.pascal_voc_io import PascalVocReader
+from libs.pascal_voc_io import XML_EXT
 
 
-img = cv2.imread("messi.jpg")[:,:,::-1] #OpenCV uses BGR channels
-bboxes = pkl.load(open("messi_ann.pkl", "rb"))
+def loadPascalXMLByFilename(xmlPath):
+    if xmlPath is None:
+        return
+    if os.path.isfile(xmlPath) is False:
+        return
 
+    # self.set_format(FORMAT_PASCALVOC)
 
+    tVocParseReader = PascalVocReader(xmlPath)
+    shapes = tVocParseReader.getShapes()
+    return shapes
+    
+def resizeAndCrop(raw,newSize):
+    xsize,ysize = raw.shape
+    minDimension = min((x_raw,y_raw))
+    xmin = math.floor(xsize/2-minDimension/2)
+    ymin = math.floor(ysize/2-minDimension/2)
 
-transforms = Sequence([RandomHorizontalFlip(1), RandomScale(0.2, diff = True), RandomRotate(10)])
+    width = minDimension
+    height = minDimension
 
-img, bboxes = transforms(img, bboxes)
+    cropped = raw[xmin:xmin+height,ymin:ymin+width]
+    resized = cv2.resize(cropped,(newSize,newSize),interpolation=INTER_NEAREST)
+    return resized
 
-plt.imshow(draw_rect(img, bboxes))
+def resizeBoxes(bboxes,newSize,origSize,minDimension):
+    xsize = origSize[0]
+    ysize = origSize[1]
+    newBoxes = np.zeros((bboxes.shape))
+    
+    # Transform x's
+    newBoxes[:,0] = newSize*((bboxes[:,0]+(xsize/2))-(xsize-minDimension)/2)/minDimension 
+    newBoxes[:,2] = newSize*((bboxes[:,2]+(xsize/2))-(xsize-minDimension)/2)/minDimension 
+    
+    # Transform y's
+    newBoxes[:,1] = newSize*((bboxes[:,1]+(ysize/2))-(ysize-minDimension)/2)/minDimension 
+    newBoxes[:,3] = newSize*((bboxes[:,3]+(ysize/2))-(ysize-minDimension)/2)/minDimension 
+
+    return newboxes
+
+img = cv2.imread("C:/Users/mgonzalez91/Dropbox (GaTech)/Coursework/SU20 - Digital Image Processing/AND_Project/slice_images_raw/subset_images/slice_3-14-2018_1.tiff")[:,:,::-1] #OpenCV uses BGR channels
+bboxes = loadPascalXMLByFilename("C:/Users/mgonzalez91/Dropbox (GaTech)/Coursework/SU20 - Digital Image Processing/AND_Project/slice_images_raw/subset_images/slice_3-14-2018_1.xml")
+print('bboxes = ',bboxes)
+
+newSize = 640 # side length of a square input image
+resizedI = resizeAndCrop(I,newSize)
+newBoxes = resizeBoxes(bboxes,newSize,origSize,minDimension)
+print('newboxes = ',newBoxes)
+plt.imshow(draw_rect(resizedI,newBoxes))
+plt.show()
